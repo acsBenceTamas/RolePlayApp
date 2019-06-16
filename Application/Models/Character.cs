0bbf953b7327650a5be2ee0b399080cd576ab626
acsBenceTamas <acs.bence.tamas@gmail.com>
2019-06-16 13:08:22 +0200

    Add project files.

---
+++ Application/Models/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class Character
+    {
+        /// <summary>
+        /// Name of the character.
+        /// </summary>
+        public string Name { get; set; }
+        
+        /// <summary>
+        /// Color code of the character in RGB hex code.
+        /// </summary>
+        public string ColorCode { get; private set; }
+
+        private Color
+
+        public Character(string name)
+        {
+            Name = name;
+        }
+    }
+}
