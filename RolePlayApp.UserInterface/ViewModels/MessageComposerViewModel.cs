fa1bfb8b4be7b94d763aa40a5bea9f37bfe9e160
acsBenceTamas <acs.bence.tamas@gmail.com>
2019-06-26 23:03:13 +0200

    MessageItemType selection shows better

---
+++ RolePlayApp.UserInterface/ViewModels/MessageComposerViewModel.cs
@@ -0,0 +1,19 @@
+﻿using RolePlayApp.Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RolePlayApp.UserInterface.ViewModels
+{
+    class MessageComposerViewModel
+    {
+        public MessageComposerViewModel (MessageComposer composer)
+        {
+            Composer = composer;
+        }
+
+        public MessageComposer Composer { get; private set; }
+    }
+}
